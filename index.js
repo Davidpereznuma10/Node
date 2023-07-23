f06123524d8c24dd3fc940c589e1a5a9f70b854f
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-23 07:13:51 -0500

    feat :globe_with_meridians: Creacion del server
    Se crea el localhost en el puerto 3000 para su manipulacion.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello world!');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req,res)=>{
+  res.send('Hola  Mundo!');
+});
+
+app.listen(port, ()=>{
+  console.log('mi port' + port);
+});
