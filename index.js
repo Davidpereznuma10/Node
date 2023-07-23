e4595c6cf7becfd87ca4a1f7a87d9003932aacd2
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-23 07:42:15 -0500

    feat :sparkles: rutas
    Creamos algunas rutas como ejemplo para visualizar el resultado en el navegador.

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ app.get('/', (req,res)=>{
   res.send('Hola  Mundo!');
 });
 
+app.get('/nueva1', (req,res)=>{
+  res.send('SOY LA RUTA NUMERO 1');
+});
+
+app.get('/nueva2', (req,res)=>{
+  res.json({
+    nombre:'David',
+    edad: 23,
+    empleo: 'dev'
+  });
+});
+
+
 app.listen(port, ()=>{
   console.log('mi port' + port);
 });
