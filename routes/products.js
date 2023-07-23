a632802b2603c3b1ae2c3d915d7ddc1e685ecc45
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-23 17:10:25 -0500

    feat 🧱  servcios de productos
    Estamos trasladando toda la logica a los servicios que es donde debe estar, esto se hace para que cada una tenga su propia logica por separado y que las rutas no tengan codigo que almacenar, esto hace que nuestro codigo sea mas organizao y facil de mantener.

--- routes/products.js
+++ routes/products.js
@@ -1,77 +1,42 @@
-const express = require("express");
-const router  = express.Router();
-const { faker } = require('@faker-js/faker');
+const express = require('express');
+
+const ProductsService = require('../services/products_service');
+
+const router = express.Router();
+const service = new ProductsService();
 
 router.get('/', (req, res) => {
-  const products =[];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 1; i <= limit; i++) {
-    products.push({
-      id:i,
-      name: faker.commerce.productName(),
-      price: parseInt(faker.commerce.price(), 10),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products)
+  const products = service.find();
+  res.json(products);
 });
 
-router.get('/filter',(req, res)=>{
-  res.send('prueba-filtro');
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filter');
 });
 
 router.get('/:id', (req, res) => {
-    const { id } = req.params;
-    if(id === '999'){
-      res.status(404).json({
-        message:'not Found'
-      });
-    }else{
-      res.status(200).json({
-        id,
-        name: 'iPhone X3',
-        price: 32000,
-    });
-    }
+  const { id } = req.params;
+  const product = service.findOne(id);
+  res.json(product);
 });
 
-router.post("/",(req,res)=>{
+router.post('/', (req, res) => {
   const body = req.body;
-  res.status(201).json({
-    message:'created',
-    data: body
-  });
-})
+  const newProduct = service.create(body);
+  res.status(201).json(newProduct);
+});
 
-router.patch('/:id', (req, res)=>{
+router.patch('/:id', (req, res) => {
   const { id } = req.params;
   const body = req.body;
-  res.json({
-    message: 'update',
-    data: body,
-    id,
-  });
-})
+  const product = service.update(id, body);
+  res.json(product);
+});
 
-router.put('/:id', (req, res)=>{
+router.delete('/:id', (req, res) => {
   const { id } = req.params;
-  const body = req.body;
-  res.json({
-    message: 'Actualiced',
-    data: body,
-    id,
-  });
-})
-
-router.delete('/:id', (req, res)=>{
-  const { id } = req.params;
-  res.json({
-    message: 'deleted',
-    id,
-  });
-})
-
-
+  const rta = service.delete(id);
+  res.json(rta);
+});
 
 module.exports = router;
