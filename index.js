190ebe6fa6d47de6b75c17af1f81afa32e0a3286
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-23 09:40:49 -0500

    feat :sparkles: Rutas
    En esta ocacion creamos diferentes rutas para el manejo de peticiones dentro de la base de datos.
    Esto no ayuda a manejar los errores y peticiones combinandolos para que de esta forma sea mucho mas preciso.

--- index.js
+++ index.js
@@ -1,25 +1,71 @@
-const express = require('express');
-
+const express = require("express");
 const app = express();
-const port = 3000;
+const PORT = 3000;
 
-app.get('/', (req,res)=>{
-  res.send('Hola  Mundo!');
+app.get('/', (req, res) => {
+    res.send('Hello crayola');
 });
 
-app.get('/nueva1', (req,res)=>{
-  res.send('SOY LA RUTA NUMERO 1');
+app.get('/route-new', (req, res) => {
+    res.send('Hola soy una nueva ruta o endpoint');
 });
 
-app.get('/nueva2', (req,res)=>{
-  res.json({
-    nombre:'David',
-    edad: 23,
-    empleo: 'dev'
-  });
+app.get('/products', (req, res) => {
+    res.json([{
+            name: 'Laptop Gamer',
+            price: 23000,
+        },
+        {
+            name: 'iPhone X3',
+            price: 32000,
+        }
+    ]);
 });
 
-
-app.listen(port, ()=>{
-  console.log('mi port' + port);
+app.get('/products/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({
+        id,
+        name: 'iPhone X3',
+        price: 32000,
+    });
 });
+
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+    const { categoryId, productId } = req.params;
+    res.json({
+        categoryId,
+        productId
+    });
+});
+
+app.get('/categories/:categoryId', (req, res) => {
+    const { categoryId } = req.params;
+    res.json({
+        categoryId,
+        category: 'Computers & Accesories'
+    });
+});
+
+app.get('/people', (req, res) => {
+    res.json([{
+        name: 'Arturo',
+        type: 'employee'
+    }, {
+        name: 'Jimena',
+        type: 'customer'
+    }]);
+});
+
+app.get('/people/:id', (req, res) => {
+    const { id } = req.params;
+    res.json({
+        id,
+        name: 'Arturo',
+        type: 'employee'
+    });
+});
+
+app.listen(PORT, () => {
+    console.log('Funciona en el puerto: ' + PORT);
+})
