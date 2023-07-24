5de1faa1d6c496b967bb251b482e227acd8bef47
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-23 23:14:07 -0500

    feat 🧱 servicios de usuarios
    Se crean los serviciode usuarios para apartar la logica de las rutas y hacer mas facil su mantenimiento.

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,36 @@
 const express = require("express");
+const userService = require("../services/user_service");
+const services = new userService()
 const router = express.Router();
 
-router.get('/', (req,res)=>{
-  const { limit , offset } = req.query;
-  if(limit && offset){
-    res.json({
-      limit,
-      offset
-    });
-  }else
-  res.send('No hay parametros');
-})
+router.get('/',(req,res)=>{
+  const user = services.find();
+  res.json(user);
+});
+
+router.get('/:id', (req,res)=>{
+  const { id } = req.params;
+  const user = services.finOne(id);
+  res.json(user);
+});
+
+router.post('/', (req,res)=>{
+  const body = req.body;
+  const newUser = services.create(body);
+  res.status(201).json(newUser);
+});
+
+router.patch('/:id', (req,res)=>{
+  const { id } = req.params;
+  const body = req.body;
+  const upUser = services.update(id, body);
+  res.json(upUser);
+});
+
+router.delete('/', (req,res)=>{
+  const { id } = req.params;
+  const delUser = services.delete(id);
+  res.json(delUser);
+});
 
 module.exports = router;
