b4c0c9f98865e872cc36cdbd1ada6c3ccbac8688
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-23 22:37:26 -0500

    feat: :bricks: servicios de personas
    Se crean los servicios para para separ la logica de las rutas y hacer mas facil su mantenimiento

--- routes/people.js
+++ routes/people.js
@@ -1,29 +1,38 @@
 const express = require('express');
+const peplesService = require('../services/people_service');
+const peopleService = require('../services/people_service');
 const router = express.Router();
-const { faker } = require('@faker-js/faker');
+const service = new peopleService();
+
 
 router.get('/', (req, res) => {
-  const peoples = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let i = 0; i < limit; i++) {
-    peoples.push({
-      id:i,
-      name: faker.person.firstName(),
-      zodiaco: faker.person.zodiacSign(),
-      edad: faker.number.int({min:18 ,max: 80}),
-    });
-  }
-  res.json(peoples)
+ const people = service.find();
+  res.json(people)
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  res.json({
-      id,
-      name: 'Arturo',
-      type: 'employee'
-  });
+  const people = service.findOne(id);
+  res.json(people);
+});
+
+router.post('/',(req, res)=>{
+  const body = req.body;
+  const newPeople = service.create(body)
+  res.status(201).json(newPeople);
+})
+
+router.patch('/:id', (req, res)=>{
+  const { id }= req.params;
+  const body = req.body;
+  const upPeople = service.update(id, body)
+  res.json(upPeople);
+})
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  const deletedPerson = service.delete(id);
+  res.json(deletedPerson);
 });
 
 module.exports = router;
---
+++ services/people_service.js
@@ -0,0 +1,63 @@
+const { faker } = require('@faker-js/faker');
+
+class peopleService{
+constructor(){
+  this.peoples=[];
+  this.generate();
+}
+
+  generate(){
+    const limit = 20;
+    for (let i = 0; i < limit; i++) {
+      this.peoples.push({
+        id: faker.datatype.uuid(),
+        name: faker.person.firstName(),
+        zodiaco: faker.person.zodiacSign(),
+        edad: faker.number.int({min:18 ,max: 80}),
+      });
+    }
+  }
+
+  create(){
+    const { name, zodiaco, edad } =data;
+    const people = {
+      id: faker.datatype.uuid(),
+      name,
+      zodiaco,
+      edad,
+    };
+    this.peoples.push(people);
+    return people;
+  }
+
+  find(){
+    return this.peoples
+  }
+
+  findOne(id){
+    return this.peoples.find((item)=> item.id === id);
+  }
+  update(id, changes){
+    const index = this.peoples.findIndex((item)=> item.id === id);
+    if (index===-1) {
+      throw new Error('La persona no existe');
+    }
+    const people = this.peoples[index];
+    this.peoples[index]={
+      ...people,
+      ...changes,
+    };
+    return this.peoples[index];
+  }
+
+  delete(id) {
+    const index = this.peoples.findIndex((item) => item.id === id);
+    if (index === -1) {
+      throw new Error('Product not found');
+    }
+    this.peoples.splice(index, 1);
+    return { id };
+  }
+}
+
+module.exports= peopleService;
