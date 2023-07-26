70f79c915fde405dcec6ec31fd5e11245e2372b4
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-26 09:48:43 -0500

    feat: :building_construction: Cambio de estructura
    Cambiamos la estructura cambiando el nombre de una carpeta, ademas de actualizar las rutas de los archivos,

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 const express = require("express");
-const routerApi = require('./routes');
+const routerApi = require('./routers');
+const { logErrors, errorHanlder, boomError }=require('./middlewares/error.handler')
 
 const app = express();
+
 const PORT = 3001;
 
+//Middleware que se utiliza para analizar el cuerpo de las solicitudes entrantes en formato JSON
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello crayola');
-});
-
-app.get('/route-new', (req, res) => {
-    res.send('Hola soy una nueva ruta o endpoint');
-});
-
+//Llama las rutas para hacer las peticiones
 routerApi(app);
 
+//Importacion de Middlewares para captar errores
+app.use(logErrors)
+app.use(boomError)
+app.use(errorHanlder)
+
+//Puerto que usamos he imprimimos
 app.listen(PORT, () => {
     console.log('Funciona en el puerto: ' + PORT);
 })
--- routers/index.js
+++ routers/index.js
@@ -4,11 +4,14 @@ const categories = require('./categories.js');
 const people = require('./people.js');
 const express = require('express');
 
+//Ruta api
 function routerApi(app){
 
+  //version de la api
   const router = express.Router();
   app.use('/api/v1', router)
 
+  //Rutas de la api
   router.use('/products', products);
   router.use('/users', users);
   router.use('/categories',categories);
