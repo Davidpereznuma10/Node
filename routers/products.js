a8edaac6549ac617cf3345e909d7c9c5fd327533
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-07-27 09:22:52 -0500

    feat: :sparkles: Dto products
    Se agrega dto para validacin de parametros y maenejo de erorres, se implementa en la ruta por medio de un middleware

--- routers/products.js
+++ routers/products.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const ProductsService = require('../services/products_service');
-const { checkEmptyJSON } = require('../middlewares/error.handler')
+const validatorHandler = require('../middlewares/validator.handler');
+const { createProductDto, updateProductDto, getProductDto  } = require('../model/product_Dto');
 const router = express.Router();
 const service = new ProductsService();
 
@@ -15,10 +16,12 @@ try {
 });
 
 //Ruta para traer a productos por id
-router.get('/:id_product', async(req, res, next) => {
-  const { id_product } = req.params;
+router.get('/:id',
+validatorHandler(getProductDto, 'params'),
+  async(req, res, next) => {
+  const { id } = req.params;
   try {
-    const product = await service.findOne(id_product);
+    const product = await service.findOne(id);
     res.status(200).json(product);
   } catch (error) {
     next(error);
@@ -26,7 +29,9 @@ router.get('/:id_product', async(req, res, next) => {
 });
 
 // Ruta para crear un nuevo producto
-router.post('/',checkEmptyJSON, async(req, res, next) => {
+router.post('/',
+  validatorHandler(createProductDto,'body'),
+  async(req, res, next) => {
   const body = req.body;
   try {
     const newProduct = await service.create(body);
@@ -37,11 +42,14 @@ router.post('/',checkEmptyJSON, async(req, res, next) => {
 });
 
 //Ruta para actualizar a las productos
-router.patch('/:id_product', checkEmptyJSON, async(req, res, next) => {
-  const { id_product } = req.params;
+router.patch('/:id',
+  validatorHandler(getProductDto, 'params'),
+  validatorHandler(updateProductDto,'body'),
+  async(req, res, next) => {
+  const { id } = req.params;
   const body = req.body;
   try {
-    const product = await service.update(id_product, body);
+    const product = await service.update(id, body);
     res.json(product);
   } catch (error) {
     next(error);
@@ -49,10 +57,11 @@ router.patch('/:id_product', checkEmptyJSON, async(req, res, next) => {
 });
 
 //Ruta Para borrar a productos por id
-router.delete('/:id_product', async(req, res, next) => {
-  const { id_product } = req.params;
+router.delete('/:id',
+  async(req, res, next) => {
+  const { id } = req.params;
   try {
-    const rta = await service.delete(id_product);
+    const rta = await service.delete(id);
     res.json(rta);
   } catch (error) {
     next(error);
