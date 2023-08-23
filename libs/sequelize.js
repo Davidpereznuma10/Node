566e6f3eb9c41d71555e1401f225d08082e1a366
Davidpereznuma10 <jdpereznuma@gmail.com>
2023-08-23 09:27:42 -0500

    feat :construction: Se hace coneccion con la db
    Se hace connecion atravez de sequelize  utlizando los modelos que exportamos desde la db, esto nos genera un estandar para poder manipularlo mas facilmente y de manera mas general.

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,15 +1,19 @@
 const { Sequelize } = require('sequelize');
 
-const { config } = require('../config/config');
+const { config } = require('./../config/config');
+const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-    dialect:'postgres',
-    logging: true,
+  dialect: 'postgres',
+  logging: false,
 });
 
-module.exports= { sequelize };
+setupModels(sequelize);
 
+sequelize.sync();
+
+module.exports = sequelize;
